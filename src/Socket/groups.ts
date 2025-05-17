7103d99d6fd40ac64eb4b5d28fac15c6029ac358
X-POKE <mr.familia13@gmail.com>
2025-05-17 15:34:23 +0700

    chore: change string to 'pn' | 'lid' in addresingMode

--- src/Socket/groups.ts
+++ src/Socket/groups.ts
@@ -339,7 +339,7 @@ export const extractGroupMetadata = (result: BinaryNode) => {
 	const memberAddMode = getBinaryNodeChildString(group, 'member_add_mode') === 'all_member_add'
 	const metadata: GroupMetadata = {
 		id: groupId,
-		addressingMode: group.attrs.addressing_mode,
+		addressingMode: group.attrs.addressing_mode as 'pn' | 'lid',
 		subject: group.attrs.subject,
 		subjectOwner: group.attrs.s_o,
 		...(group.attrs.addressing_mode === "lid" ? { subjectOwnerPn: group.attrs.s_o_pn } : {}),
